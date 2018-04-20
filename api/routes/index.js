ee46dd1e010ed97ff6c767b3797203cd1dbcfa89
Vita Karas <akaaa1606@gmail.com>
2018-04-20 17:55:26 +0300

    use passport for authentification

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,8 +1,17 @@
 let express = require('express');
 let router = express.Router();
+let jwt = require('express-jwt'); //lets you authenticate HTTP requests using JWT tokens in your Node.js applications
 
-router.get('/', function (req, res) {
-    res.send("Hello");
-} );
+let auth = jwt({
+    secret: 'MY_SECRET',
+    userProperty: 'payload'
+});
+
+let ctrlProfile  = require('../controllers/profile');
+let ctrlAuth = require('../controllers/authentication');
+
+router.get('/profile',auth, ctrlProfile);
+router.post('/login', ctrlAuth.login);
+router.post('/register', ctrlAuth.register);
 
 module.exports = router;
